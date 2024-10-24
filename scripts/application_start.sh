a4bdb4e58b82703185d5f824b4336b00c39ff8ec
Joedahms <dahms.joejoe@gmail.com>
2024-10-24 18:38:12 -0400

    Added appspec.yml and required scripts for codedeploy

---
+++ scripts/application_start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ec2-user/trustworthy-module-registry
+
+# Navigate into our working directory where we have all our github files
+cd /home/ec2-user/trustworthy-module-registry
+
+# Add npm and node to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # Loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # Loads nvm bash_completion (node is in path now)
+
+# Install node modules
+npm install
+
+./run sample-file.txt &
