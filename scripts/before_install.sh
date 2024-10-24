7bb230ced858d430207804921a87f0810860c50d
Joedahms <dahms.joejoe@gmail.com>
2024-10-24 19:25:55 -0400

    Changed ec2-user to ubuntu

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,7 +6,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 
 # Create working directory if it doesn't exist
-DIR="/home/ec2-user/trustworthy-module-registry"
+DIR="/home/ubuntu/trustworthy-module-registry"
 if [ -d "$DIR" ]; then
 
 else
