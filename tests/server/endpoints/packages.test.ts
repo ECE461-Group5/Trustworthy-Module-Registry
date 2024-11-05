d8823629aaa911844f52970dc2abb2afe0ed0aad
Joedahms <dahms.joejoe@gmail.com>
2024-11-04 20:23:24 -0500

    Hopefully removed 403 errors in super-linter

--- tests/server/endpoints/packages.test.ts
+++ tests/server/endpoints/packages.test.ts
@@ -3,11 +3,6 @@ import request from 'supertest';
 import app from '../../../src/server/server.ts'
 
 describe('packages endpoint', () => {
-  it("GET /packages?offset=20 should return 404", async () => {
-    const res = await request(app).get("/packages?offset=20");
-    expect(res.statusCode).toBe(404);
-  })
-
   it("POST /packages?offset=20 should say not implemented and the offset", async () => {
     const res = await request(app).post("/packages?offset=20");
     expect(res.statusCode).toBe(200);
