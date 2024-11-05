3c4d696aea2c0569b333c9ac0f09b3cc41700927
Joedahms <39975773+Joedahms@users.noreply.github.com>
2024-11-05 18:29:43 -0500

    yaml parser
    Signed-off-by: Joedahms <39975773+Joedahms@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 // eslint.config.js
+import yamlParser from 'yaml-eslint-parser'
+
 export default [
     {
         rules: {
             "semi": "error",
         },
         languageOptions: {
-            parser: 'yaml-eslint-parser'
+            parser: yamlParser,
         },
     }
 ];
