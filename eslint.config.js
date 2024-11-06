b3469424aee7e974dc4eaea3a29d712c363199a5
Joedahms <39975773+Joedahms@users.noreply.github.com>
2024-11-05 19:23:02 -0500

    Update eslint.config.js
    Signed-off-by: Joedahms <39975773+Joedahms@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,9 @@
 // eslint.config.js
-//import yamlParser from 'yaml-eslint-parser';
 
 export default [
     {
         rules: {
             "semi": "error",
-        },
-        languageOptions: {
-//            parser: yamlParser,
         },
     }
 ];
