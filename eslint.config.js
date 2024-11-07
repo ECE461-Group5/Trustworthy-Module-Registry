82542037230d1914d6cda1579e17620e63670cdb
Joedahms <39975773+Joedahms@users.noreply.github.com>
2024-11-06 20:01:49 -0500

    eslint config
    Signed-off-by: Joedahms <39975773+Joedahms@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,19 @@
+import js from "@eslint/js";
+
 export default [
-  extends: ["eslint:recommended"],
-  overrides: [
-    {
-      files: ["**/*.ts", "**/*.tsx"],
-      extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-      env: {
-        "node": true,
-        "es2021": true
-      },
-      parser: "@typescript-eslint/parser",
-      plugins: ["@typescript-eslint"],
-      rules: {
-        semi: "error",
-        quotes: ["error", "single"],
-      }
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    env: {
+      "node": true,
+      "es2021": true
+    },
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    rules: {
+      semi: "error",
+      quotes: ["error", "single"],
     }
-  ]
+  }
 ];
+
