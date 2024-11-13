e691ea4977f2536cb9d94106e8417a1fc23e8a2c
Joedahms <dahms.joejoe@gmail.com>
2024-11-12 20:20:01 -0500

    Playing around with processing the body of a request

--- src/server/controllers/packagesController.ts
+++ src/server/controllers/packagesController.ts
@@ -1,10 +1,31 @@
+/*
+ * Author(s): Joe Dahms
+ * Purpose: Handle incoming requests to the /packages endpoint
+*/
+
 import { Request, Response } from "express";
 
-interface RequestQuery {
+interface MyObj {
+  Name: string
+  Version: string
+}
+
+interface que {
   offset?: string;
 }
 
-export const getPackages = /*async*/ (req: Request<unknown, unknown, unknown, RequestQuery>, res: Response): void => {
+export const getPackages = (req: Request<{},{}, MyObj[], que>, res: Response): void => {
+
+  const reqBody = req.body;
   const offset = req.query.offset;
-  res.json({ message: "NOT IMPLEMENTED: get packages", offset: `${offset}` });
+
+  //console.log(JSON.stringify(reqBody));
+
+  if (Array.isArray(reqBody) && reqBody.length > 0) {
+    reqBody.forEach((obj, index) => {
+      console.log(`Package ${index + 1}: Name = ${obj.Name}, Version = ${obj.Version}`);
+    });
+  }
+
+  res.json({ test: reqBody, offset: offset });
 };
