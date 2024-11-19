14fb41b0cf28f7461c5a253dc07d29d34b4c8d3c
Joedahms <dahms.joejoe@gmail.com>
2024-11-18 19:32:08 -0500

    return package dummy responses

--- tests/server/endpoints/package.test.ts
+++ tests/server/endpoints/package.test.ts
@@ -11,19 +11,88 @@ describe("/package endpoint", () => {
 });
 
 
-describe("/package/:id endpoint", () => {
-  it("GET /package/:id is not implemented", async () => {
-    const res = await request(app).get("/package/:id");
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toHaveProperty("message", "NOT IMPLEMENTED: get package");
+describe("GET /package/:id endpoint", () => {
+  // Return package
+  test.each([
+    {
+      testName: "Return the package",
+      packageID: "00000000",
+      expectedStatus: 200,
+      expectedBody: 
+      {
+        "metadata": {
+          "Name": "<string>",
+          "Version": "<string>",
+          "ID": "00000000"
+        },
+        "data": {
+          "Content": "<string>",
+          "URL": "<string>",
+          "debloat": "<boolean>",
+          "JSProgram": "<string>"
+        }
+      },
+    },
+  ])("$testName", async ({ packageID, expectedStatus, expectedBody }) => {
+    const response = await request(app)
+      .get(`/package/${packageID}`);
+
+    expect(response.statusCode).toEqual(expectedStatus);
+    expect(response.body).toEqual(expectedBody);  
   });
 
+  // Package ID format
+  test.each([
+    {
+      testName: "Package ID one digit too short",
+      packageID: "1234567",
+      expectedStatus: 400,
+      expectedBody: {},
+    },
+    {
+      testName: "Package ID one digit too long",
+      packageID: "123456789",
+      expectedStatus: 400,
+      expectedBody: {},
+    },
+  ])("$testName", async ({ packageID, expectedStatus, expectedBody }) => {
+    const response = await request(app)
+      .get(`/package/${packageID}`);
+
+    expect(response.statusCode).toEqual(expectedStatus);
+    expect(response.body).toEqual(expectedBody);  
+  });
+
+  // Authentication
+ 
+  // Package does not exist
+  test.each([
+    {
+      testName: "Package does not exist",
+      packageID: "99999999",
+      expectedStatus: 404,
+      expectedBody: {},
+    },
+  ])("$testName", async ({ packageID, expectedStatus, expectedBody }) => {
+    const response = await request(app)
+      .get(`/package/${packageID}`);
+
+    expect(response.statusCode).toEqual(expectedStatus);
+    expect(response.body).toEqual(expectedBody);  
+  });
+});
+  
+
+
+describe("PUT /package/:id endpoint", () => {
   it("PUT /package/:id is not implemented", async () => {
     const res = await request(app).put("/package/:id");
     expect(res.statusCode).toBe(200);
     expect(res.body).toHaveProperty("message", "NOT IMPLEMENTED: update package");
   });
+});
 
+describe("DELETE /package/:id endpoint", () => {
   it("DELETE /package/:id is not implemented", async () => {
     const res = await request(app).delete("/package/:id");
     expect(res.statusCode).toBe(200);
