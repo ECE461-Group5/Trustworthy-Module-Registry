ebd56e40f11fc89584f1d26d3bcfd7933e6dea01
peace code <peacecode@peaces-iMac.local>
2024-11-20 13:57:38 -0500

    updated the main navigation bar

---
+++ src/frontend/src/MainNavbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function MainNavbar() {
+    return (
+        <div className="navbar">
+            <a href="/home">
+                <div className="logo">
+                    <img src="/purdue_logo.PNG" alt="Home" />
+                </div>
+            </a>
+        </div>
+    );
+}
+
+export default MainNavbar;
