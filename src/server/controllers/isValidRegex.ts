c27c3060e7f1780c0cd35fc50da4c4bb8e4fdcf8
Joedahms <dahms.joejoe@gmail.com>
2024-11-20 13:51:19 -0500

    fixed regex

--- src/server/controllers/isValidRegex.ts
+++ src/server/controllers/isValidRegex.ts
@@ -3,9 +3,12 @@
  * Purpose: Check if a regular expression is a valid JavaScript regular expression.
 */
 
+import { escapeRegExp } from "lodash";
+
 export function isValidRegex (pattern: string): boolean {
   try {
-    new RegExp(pattern);
+    const safePattern = escapeRegExp(pattern);
+    new RegExp(safePattern);
     return true;
   }
   catch (error: unknown) {
