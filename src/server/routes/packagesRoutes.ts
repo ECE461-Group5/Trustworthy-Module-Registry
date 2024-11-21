0393329d97b3c0fb886544e7f025de8fb25c7f41
Joedahms <dahms.joejoe@gmail.com>
2024-11-21 13:54:21 -0500

    added more file heading comments and removed some unused code from the previous group

--- src/server/routes/packagesRoutes.ts
+++ src/server/routes/packagesRoutes.ts
@@ -1,8 +1,14 @@
-import express from "express";
-const router = express.Router();
+/*
+ * Author(s): Joe Dahms
+ * Purpose: Set up the routes for the packages endpoint. See controllers
+ * for request handling logic.
+ */
 
+
+import express from "express";
 import { getPackages } from "../controllers/packagesController.js";
 
+const router = express.Router();
 router.post("/", getPackages);
 
 export default router;
