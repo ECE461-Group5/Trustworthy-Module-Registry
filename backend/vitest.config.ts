8c9c37a8a6fe095d0db6247bf5379d51ad4a5568
Geromy <geromycunningham@gmail.com>
2024-11-23 22:14:16 -0500

    updated config again

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -4,12 +4,12 @@ export default defineConfig({
   test: {
     globals: true, // Use global test APIs like `describe`, `it`, etc.
     environment: "node", // Set the test environment to Node.js
-    include: ["backend/tests/**/*.test.ts"], // Specify the test files
+    include: ["tests/**/*.test.ts"], // Specify the test files
     coverage: {
       enabled: true, // Enable coverage reports
       reporter: ["json-summary"], // Generate coverage reports
-      include: ["backend/**/*.ts"],
-      exclude: ["backend/server/**/*.ts"],
+      include: ["**/*.ts"],
+      exclude: ["server/**/*.ts"],
       reportsDirectory: "src/utils/reports/coverage",
     },
   },
