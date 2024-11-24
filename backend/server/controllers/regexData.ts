da82f92be23843308c499a0830241cfa1d817579
Geromy <geromycunningham@gmail.com>
2024-11-23 20:37:28 -0500

    Merge branch 'main' into 106-frontend-final

---
+++ backend/server/controllers/regexData.ts
@@ -0,0 +1,8 @@
+/*
+ * Author(s): Joe Dahms
+ * Purpose: Data type representing Regex Data. Improve transfer of data between Express server and database
+ */
+
+export interface RegexData {
+  RegEx: string;
+}
--- backend/server/routes/packagesRoutes.ts
+++ backend/server/routes/packagesRoutes.ts
@@ -4,7 +4,6 @@
  * for request handling logic.
  */
 
-
 import express from "express";
 import { getPackages } from "../controllers/packagesController.js";
 
