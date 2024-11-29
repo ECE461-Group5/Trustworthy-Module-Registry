037c6bbbd97670a6e33ff2394fcaf08b946148d2
Geromy <geromycunningham@gmail.com>
2024-11-29 16:52:27 -0500

    edited files so application should deploy automatically on EC2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,6 +12,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # Loads nvm bash_completion (node is in path now)
 
 # Install node modules
-npm install
+# NOTES: scripts in root package.json  --  "wininstall" works for windows, "ec2install" works for linux
+npm run ec2install
 
-./run sample-file.txt >app.out.log 2>app.err.log </dev/null &
+# Runs start script which will build, move client for Nginx, and start the server
+./run start >app.out.log 2>app.err.log </dev/null &
