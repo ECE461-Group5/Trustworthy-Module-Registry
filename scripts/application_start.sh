fb4984e51b335a0380d83dbd88915f534c36e1ab
Geromy <geromycunningham@gmail.com>
2024-11-29 18:52:47 -0500

    fixed a typo

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -16,7 +16,7 @@ export NVM_DIR="$HOME/.nvm"
 # Build FE & BE, start server
 (cd ./backend && npm run build && npm run start) && (cd ./client && npm run build) >app.out.log 2>app.err.log </dev/null &
 # Copy the "build" folder to "/var/www/html" for Nginx, reload Nginx
-(cd ./client && sudo cp -R build/ /var/www/html/front-end/) && sudo systemctl reload nginx >app.out.log 2>app.err.log </dev/null &
+(cd ./client && sudo cp -R build/ /var/www/html/front-end/) && (sudo systemctl reload nginx) >app.out.log 2>app.err.log </dev/null &
 
 # Runs start script which will build, move client for Nginx, and start the server
 # ./run start >app.out.log 2>app.err.log </dev/null &
