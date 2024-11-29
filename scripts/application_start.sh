1591f482affb4bf95fa8fbc9ac68f8f0be71f44e
Geromy <geromycunningham@gmail.com>
2024-11-29 18:52:00 -0500

    updated application_start to make the ./run in root no longer needed.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,7 +12,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # Loads nvm bash_completion (node is in path now)
 
 # Install node modules
-npm run postinstall
+(cd ./backend && npm install) && (cd ./client && npm install) >app.out.log 2>app.err.log </dev/null &
+# Build FE & BE, start server
+(cd ./backend && npm run build && npm run start) && (cd ./client && npm run build) >app.out.log 2>app.err.log </dev/null &
+# Copy the "build" folder to "/var/www/html" for Nginx, reload Nginx
+(cd ./client && sudo cp -R build/ /var/www/html/front-end/) && sudo systemctl reload nginx >app.out.log 2>app.err.log </dev/null &
 
 # Runs start script which will build, move client for Nginx, and start the server
-./run start >app.out.log 2>app.err.log </dev/null &
+# ./run start >app.out.log 2>app.err.log </dev/null &
