9d592082b63fb0490658e0477424167fda20a87c
dja1443 <dalmabouada@ivytech.edu>
2024-11-30 00:52:54 -0500

    CORS #2

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 // this will make your life easy due to our brother Abu Bakr
 
 const port = 443; // port for https
-const route = 'https://ec2-18-206-89-253.compute-1.amazonaws.com';
+const route = 'https://ec2-52-91-187-37.compute-1.amazonaws.com';
 export const config = {
     appRoute: `${route}:${port}`,
 };
