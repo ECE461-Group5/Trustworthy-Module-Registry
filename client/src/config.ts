f249c4f74d60e83506068efb1d26cbb2a42d6b6c
dja1443 <dalmabouada@ivytech.edu>
2024-12-01 09:46:16 -0500

    tried http

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 // this will make your life easy due to our brother Abu Bakr
 
-const port = 443; // 443 port# for https or 80 for http
-const route = 'https://ec2-3-84-204-34.compute-1.amazonaws.com';// be careful this may get changed every time your start/stop the AWS instance
+const port = 80; // 443 port# for https or 80 for http
+const route = 'ec2-18-233-10-161.compute-1.amazonaws.com';// be careful this may get changed every time your start/stop the AWS instance
 export const config = {
     appRoute: `${route}:${port}`,
 };
