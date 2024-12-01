4e2b3e1b663bcd24cc82206342d0221de3722580
dja1443 <dalmabouada@ivytech.edu>
2024-11-30 21:17:26 -0500

    tried hhtps/http

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 // this will make your life easy due to our brother Abu Bakr
 
-const port = 80; // port for https
-const route = 'https://ec2-3-84-19-157.compute-1.amazonaws.com';
+const port = 443; // 443 port# for https
+const route = 'https://ec2-3-84-204-34.compute-1.amazonaws.com';
 export const config = {
     appRoute: `${route}:${port}`,
 };
