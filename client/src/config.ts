e4057c89c0b71d3338e72b7f1e74fe20fb348a2d
dja1443 <dalmabouada@ivytech.edu>
2024-11-30 21:45:32 -0500

    CORS problem

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 // this will make your life easy due to our brother Abu Bakr
 
-const port = 443; // 443 port# for https
+const port = 443; // 443 port# for https or 80 for http
 const route = 'https://ec2-3-84-204-34.compute-1.amazonaws.com';
 export const config = {
     appRoute: `${route}:${port}`,
