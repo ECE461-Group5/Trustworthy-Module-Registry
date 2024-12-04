d363ac80c1401a78c5537bff5b06c67be8e10550
Joedahms <dahms.joejoe@gmail.com>
2024-12-04 17:03:39 -0500

    going to split tests up

--- backend/src/database/testing/deleteTestPackage.ts
+++ backend/src/database/testing/deleteTestPackage.ts
@@ -3,11 +3,12 @@
  * Purpose: Delete test packages from database after running a test.
  */
 
-export const deleteContentPackage = async (packageId: string): Promise<> => {
-  const deleteUser = await prisma.user.delete({
+export const deleteContentPackage = async (packageId: string): Promise<void> => {
+  await prisma.user.delete({
   where: {
     id: packageId,
   },
 });
+  return;
 };
 
