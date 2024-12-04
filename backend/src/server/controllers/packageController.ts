e93e4b567326d3f6af204a95ff22373a67e9d164
jsalyers12 <salyers8411@gmail.com>
2024-12-03 19:27:07 -0500

    Update packageController.ts
    Signed-off-by: jsalyers12 <salyers8411@gmail.com>

--- backend/src/server/controllers/packageController.ts
+++ backend/src/server/controllers/packageController.ts
@@ -106,7 +106,7 @@ export const updatePackage = (req: Request, res: Response): Response => {
 };
 
 // /package/:id
-export const deletePackage = async (req: Request, res: Response): Promise<void> => {
+export const deletePackage = async (req: Request, res: Response): Promise<Response> => {
   const packageIDString = req.params.id;
 
   // Validate that package ID is exactly 8 digits
