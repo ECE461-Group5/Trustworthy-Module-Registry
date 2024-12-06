534ac652a8880a91411cd4581b7f122e60897928
Joe Dahms <39975773+Joedahms@users.noreply.github.com>
2024-12-06 11:46:39 -0500

    Create log file
    Signed-off-by: Joe Dahms <39975773+Joedahms@users.noreply.github.com>

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,6 +5,9 @@ ENV_PATH=/home/ubuntu/trustworthy-module-registry/backend/.env
 # Create .env file
 touch $ENV_PATH
 
+# Create log file
+touch /home/ubuntu/trustworthy-module-registry/backend/logs/app.log
+
 # Fill out env file
 echo BUILD_PATH=$(aws ssm get-parameters --output text --region us-east-1 --name BUILD_PATH --with-decryption --query Parameters[0].Value) >> $ENV_PATH
 echo DATABASE_URL=$(aws ssm get-parameters --output text --region us-east-1 --name DATABASE_URL --with-decryption --query Parameters[0].Value) >> $ENV_PATH
