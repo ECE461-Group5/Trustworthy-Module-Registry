2ba9a5a91c48faa2b8758ffae4764ed742c6ebe7
Joedahms <dahms.joejoe@gmail.com>
2024-12-06 14:57:30 -0500

    test for isValidRegex

---
+++ backend/src/tests/server/endpoints/package/isValidRegex.test.ts
@@ -0,0 +1,27 @@
+/*
+ * Author(s): Joe Dahms
+ * Purpose: Test that isValidRegex can indentify harmful or incorrect regexes.
+ */
+
+import logger from "../../../../../logger.js";
+import { expect, describe, test } from "vitest";
+import { isValidRegex } from "../../../../server/controllers/isValidRegex.js";
+
+describe("isValidRegex()", () => {
+  test.each([
+    {
+      testName: "Invalid Regex",
+      regex: "[",
+      expectedResult: false,
+    },
+    {
+      testName: "Too long",
+      regex:
+        "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE",
+      expectedResult: false,
+    },
+  ])("$testName", ({ regex, expectedResult }) => {
+    const result = isValidRegex(regex);
+    expect(result).toEqual(expectedResult);
+  });
+});
