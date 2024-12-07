ef6225c7128395e46c798c2c3c5ced50070549de
Geromy <geromycunningham@gmail.com>
2024-12-07 12:01:26 -0500

    added notes to vite.config to explain the proxy setting in case it needs to be modified slightly.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -29,3 +29,10 @@ export default defineConfig({
     setupFiles: ["./src/tests/setupTests.ts"],
   },
 } as UserConfigExport);
+
+// Purpose: The proxy configuration allows you to redirect API requests made to your front end to a different server, thus avoiding CORS issues.
+// '/api': Specifies the path that will be intercepted. Any request starting with /api will be proxied.
+// target: Indicates where to forward the request, in this case to http://127.0.0.1:5555, assumed to be your backend server.
+// changeOrigin: Modifies the Origin header of the request to match the target URL, which can help prevent access issues with some APIs.
+// secure: Allows proxying requests over HTTP instead of HTTPS, useful for local development.
+// rewrite: Modifies the request path, e.g., /api/users becomes /users before being sent to the backend.
