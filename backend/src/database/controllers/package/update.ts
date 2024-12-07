f7a1378ce131825afa2a2d0133e532656ed1cea3
Joedahms <dahms.joejoe@gmail.com>
2024-12-06 20:52:17 -0500

    javadoc comments

--- backend/src/database/controllers/package/update.ts
+++ backend/src/database/controllers/package/update.ts
@@ -1,7 +1,22 @@
+/**
+ * @filename - update.ts
+ * @author(s) - Joe Dahms, Jonah Salyers
+ * @purpose - Update a package in the database. Updates content, url, debloat, and jsprogram.
+ */
+
 import prisma from "../../prisma.js";
 import { PackageData } from "../../../server/controllers/packageData.js";
 import { Package } from "../../../server/controllers/package.js";
 
+/**
+ * @function dbUpdatePackage 
+ *
+ * Update a package in the database. Updates content, url, debloat, and jsprogram.
+ *
+ * @param packageId - Id of the package to update
+ * @param packageData - The data to be updated in the package
+ * @returns - Void promise
+ */
 export const dbUpdatePackage = async (
   packageId: number,
   packageData: PackageData,
@@ -16,7 +31,6 @@ export const dbUpdatePackage = async (
         jsProgram: packageData.JSProgram,
       },
     });
-
     return;
 }
  catch (error: any) {
