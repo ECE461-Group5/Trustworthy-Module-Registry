7e7547a780e40c4c0afc81adcbe3a7e638ce1de1
dja1443 <dalmabouada@ivytech.edu>
2024-12-07 00:04:08 -0500

    added the name and the file purpose on the top

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,8 @@
+/*
+Author: Djamel Almabouada
+This is the main component that contains the routing for the application 
+*/
+
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
