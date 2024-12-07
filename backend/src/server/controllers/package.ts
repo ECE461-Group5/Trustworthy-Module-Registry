dbca8bf50cfde9c82b74f23f8025b01babaa31fa
Joedahms <dahms.joejoe@gmail.com>
2024-12-07 15:25:12 -0500

    nullPackageData logging

--- backend/src/server/controllers/package.ts
+++ backend/src/server/controllers/package.ts
@@ -4,6 +4,8 @@
  * @purpose - Interface for a package. Helps ensure that each field of a package is of an expected type.
  */
 
+import logger from "../../../logger.js";
+
 export interface Package {
   metadata: {
     Name: string | null;
@@ -27,13 +29,22 @@ export interface Package {
  * @returns - True if any null fields in data, false if no null fields in data
  */
 export function nullPackageData (packageToCheck: Package): boolean {
+  logger.info("Checking if package data has any null fields...");
   let nullData = false;
-  if (
-    packageToCheck.data.Content != null ||
-    packageToCheck.data.URL != null ||
-    packageToCheck.data.debloat != null ||
-    packageToCheck.data.JSProgram != null
-  ) {
+  if (packageToCheck.data.Content != null) {
+    logger.info("Content null");
+    nullData = true;
+  }
+  else if (packageToCheck.data.URL != null) {
+    logger.info("URL null");
+    nullData = true;
+  }
+  else if (packageToCheck.data.debloat != null) {
+    logger.info("debloat null");
+    nullData = true;
+  }
+  else if (packageToCheck.data.JSProgram != null) {
+    logger.info("JSProgram null");
     nullData = true;
   }
 
