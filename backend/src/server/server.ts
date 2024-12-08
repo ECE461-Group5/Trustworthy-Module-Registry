d9365100c0ebfd7e14a25cae10fb8509ce87b177
Geromy <geromycunningham@gmail.com>
2024-12-08 15:41:52 -0500

    updated server.ts adding line that is needed for testing and added build to gitignore

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -12,6 +12,9 @@ import packageRouter from "./routes/packageRoutes.js";
 const app: Express = express();
 const port = 3000;
 
+/* line below is used for Vite/FE testing */
+app.use(express.static("build"));
+
 app.use(express.json());
 app.use(express.urlencoded());
 
