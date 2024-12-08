da8cd362e1408681ae1692772993f89b860eec3c
Geromy <geromycunningham@gmail.com>
2024-12-08 14:11:52 -0500

    updated lint issues

--- client/src/components/Search.d.ts
+++ client/src/components/Search.d.ts
@@ -1,4 +1,4 @@
 import React from "react";
-export declare const searchPackage: (query: string) => Promise<import("axios").AxiosResponse<any, any>>;
+export declare const searchPackage: (query: string) => Promise<import("axios").AxiosResponse<unknown, unknown>>;
 declare const Search: React.FC;
 export default Search;
