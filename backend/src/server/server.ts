aafd85af6199c4759f6639ac0db22e7b6ed79e55
dja1443 <dalmabouada@ivytech.edu>
2024-12-07 21:48:58 -0500

    api call request

--- backend/src/server/server.ts
+++ backend/src/server/server.ts
@@ -1,42 +1,9 @@
-// /**
-//  * @filename - server.ts
-//  * @author(s) - Joe Dahms, Djamel Almabouada
-//  * @purpose - Core logic of the express server. Defines what routers are used when a certain endpoint is hit.
-//  */
+/**
+ * @filename - server.ts
+ * @author(s) - Joe Dahms, Djamel Almabouada
+ * @purpose - Core logic of the express server. Defines what routers are used when a certain endpoint is hit.
+ */
 
-// import express, { Express, Request, Response } from "express";
-// import packagesRouter from "./routes/packagesRoutes.js";
-// import resetRouter from "./routes/resetRoutes.js";
-// import packageRouter from "./routes/packageRoutes.js";
-
-// const app: Express = express();
-// const port = 3000;
-
-// import cors from "cors"; 
-// app.use(cors({ origin: "*" }));
-
-// app.use(express.json());
-// app.use(express.urlencoded());
-
-// app.get("/", (req: Request, res: Response) =>
-//   res.json({ message: "Express + TypeScript Server" }),
-// );
-// app.use("/packages", packagesRouter);
-// app.use("/reset", resetRouter);
-// app.use("/package", packageRouter);
-// app.use("/tracks", (req: Request, res: Response) =>
-//   res.json({ plannedTracks: "[none]" }),
-// );
-
-// app.use((req: Request, res: Response) => {
-//   res.status(404).json({ message: "Route not found" });
-// });
-
-// if (process.env.NODE_ENV !== "test") {
-//   app.listen({ port, address: "0.0.0.0" });
-// }
-
-// export default app;
 
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
