7957a6433af94d52b8f00b93d1419347ac4dd380
derekpetersen21 <derek.petersen2020@gmail.com>
2024-12-08 12:14:57 -0500

    Color change + Logos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ import PackageUploader from "./components/PackageUploader";
 import PackageDownloader from "./components/PackageDownloader";
 import Search from "./components/Search";
 
+
 function App() {
   return (
     <div className="App">
@@ -24,5 +25,31 @@ function App() {
     </div>
   );
 }
+  
+/*
+import React, { useState } from "react";
+import axios from "axios";
 
+const App: React.FC = () => {
+  const [message, setMessage] = useState<string>("");
+
+  const fetchMessage = () => {
+    axios
+      .get("/api/") // Proxy will rewrite this to "/"
+      .then((res) => {
+        setMessage(res.data.message); // "Express + TypeScript Server"
+      })
+      .catch((error) => {
+        console.error("Error fetching message:", error);
+      });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={fetchMessage}>Get Message</button>
+      <p>{message}</p>
+    </div>
+  );
+};
+*/
 export default App;
